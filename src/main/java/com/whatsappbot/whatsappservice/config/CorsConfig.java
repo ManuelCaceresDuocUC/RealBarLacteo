489b15b7ced45992b5006e3776688cebbc6bd699
Manuel Cáceres <manuel.caceres@duocuc.cl>
2025-05-25 19:20:54 -0400

    1

--- src/main/java/com/whatsappbot/whatsappservice/config/CorsConfig.java
+++ src/main/java/com/whatsappbot/whatsappservice/config/CorsConfig.java
@@ -6,11 +6,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
-
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOriginPatterns("*")
+            .allowedOrigins("*")
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
             .allowedHeaders("*");
     }
